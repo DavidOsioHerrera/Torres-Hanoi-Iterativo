e68918653b574bdd26aed6b5c89004fbc14c7114
DavidOsioHerrera <davidosioherrera@gmail.com>
2016-08-27 15:12:48 -0500

    Add files via upload

--- HanoiIterative.py
+++ HanoiIterative.py
@@ -1,60 +1,95 @@
 # autor: David Osio Herrera
 
-n = 16
-a = []
-b = []
-c = []
-movs = pow(2, n)
-
-for i in range(1, n+1):
-    a.append(i)
-print("torre inicial a=", a, "torre final c=", c)
-
 
 def reco(x):
+    # Funcion encaragda de recorrer los discos en los arreglos#
     r = len(x)
-    for j in range (0, r-1):
-        x[j] = x[j+1]
-    if len(x)>=1:
+    for j in range(0, r - 1):
+        # Elimina el primera dato del arreglo y mueve todos los demas una posicion adelante
+        x[j] = x[j + 1]
+        # (Deja el utlimo dato repetido)
+
+    if r >= 1:
+        # Elimina el ultimo dato del arreglo mientras exista
         x.pop()
 
 
-def swapDisc(y, z):
-    if (len(y)==0):
-        print("Mueve el disco",z[0],"a torre 1", str("%y"))
+def swapDisco(y, z):
+    # Funcion encargada de hacer el movimiento legal necesario entre dos postes
+
+    if len(y) == 0:
+        # Si el poste Y esta vacio mueve el disco superior de Z a Y
+        print("Mueve el disco", z[0], "de manera legal")
         temp = z[0]
         y.insert(0, temp)
         reco(z)
 
-    elif ((len(z) == 0) or (y[0] < z[0])):
-        print("Mueve el disco", y[0], "a torre 2", str("%z"))
+    elif (len(z) == 0) or (y[0] < z[0]):
+        # Si el poste Z esta vacio o el disco superior en Y es menor que el de Z mueve el disco superior de Y a Z
+        print("Mueve el disco", y[0], "de manera legal")
         temp = y[0]
         z.insert(0, temp)
         reco(y)
 
-    else:
-        print("Mueve el disco",z[0],"a torre 3", str("%y"))
+    else:  # Para cualquier otro escenario mueve el disco superor de Z a Y
+        print("Mueve el disco", z[0], "de manera legal")
         temp = z[0]
         y.insert(0, temp)
         reco(z)
 
 
+def hanoiSol(a, b, c):
+    # Funccion encargada de solucionar el problema para cualquier caso
+    print("Torre inicial A =", a)
+    print("Torre final C =", c)
+    print()
 
+    if n % 2 != 0:
+        # Soluciona los casos de discos impares
+        for j in range(1, movs):
+            if j % 3 == 1:
+                swapDisco(a, c)
+            if j % 3 == 2:
+                swapDisco(a, b)
+            if j % 3 == 0:
+                swapDisco(b, c)
 
+            print("A=", a)
+            print("B=", b)
+            print("C=", c)
+            print()
 
-for i in range (1, movs):
+    if n % 2 == 0:
+        # Soluciona los casos de discos impares
+        for j in range(1, movs):
 
-    if i%3 == 1:
-        swapDisc(a, c)
+            if j % 3 == 1:
+                swapDisco(a, b)
 
-    if i%3 == 2:
-        swapDisc(a, b)
+            if j % 3 == 2:
+                swapDisco(a, c)
 
-    if i%3 == 0:
-        swapDisc(b, c)
+            if j % 3 == 0:
+                swapDisco(c, b)
 
-if n%2==0:
-    c=b
-    b=[]
+            print("A=", a)
+            print("B=", b)
+            print("C=", c)
+            print()
 
-print("torre inicial a=", a, "torre final c=", c)
+    print('Hecho!')
+    print()
+    print("Torre inicial A =", a)
+    print("Torre final C =", c)
+
+n = int(input('Numero de discos: '))  # Numero de discos
+A = []  # Torre A
+B = []  # Torre B
+C = []  # Torre C
+movs = pow(2, n)  # Moviemtos necesarios para resolver el problema (2^n)
+
+for i in range(1, n + 1):
+    # Crea el numero de discos necesarios en la torre A
+    A.append(i)
+
+hanoiSol(A, B, C)
